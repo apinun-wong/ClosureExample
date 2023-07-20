186d2a5bc20bd46304e6909cba2d7ecc5b5324d8
Apinun <apinun@Apinuns-MacBook-Pro-2.local>
2023-07-20 10:40:06 +0700

    first commit

---
+++ ClosureExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ClosureExample
+//
+//  Created by Apinun on 20/7/2566 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
